0d4d3587d81deee32a1a2846e9ddf4df8389c0c7
Nourah Alradhi <50682042+Noura6867@users.noreply.github.com>
2021-07-08 09:47:25 +0300

    Add files via upload

---
+++ lesson2 2.java
@@ -0,0 +1,13 @@
+package exception;
+
+public class myexcption extends Exception {
+	public myexcption() {
+		super("The number should be between 1 and 10 ");
+		
+}
+	public myexcption(String message)
+	{
+	super ("message");
+}
+
+}
