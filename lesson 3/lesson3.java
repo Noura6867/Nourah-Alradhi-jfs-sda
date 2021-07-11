cb3af511bbca46b693b6fffa73c61992bb99b5e1
noura <noura6867@hotmail.com>
2021-07-11 10:28:32 +0300

    lesson1

---
+++ lesson 3/lesson3.java
@@ -0,0 +1,30 @@
+package employees;
+
+import java.util.Scanner;
+
+public class employee {
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+
+		 System.out.println("Enter employee1:");
+		String employee1= scan.nextLine();
+
+		System.out.println("Enter employee2:");
+		String employee2= scan.nextLine();
+
+		System.out.println("Enter employee3:");
+		String employee3= scan.nextLine();
+
+		System.out.println("Enter employee4:");
+		String employee4= scan.nextLine();
+
+		System.out.println("Enter employee5:");
+		String employee5= scan.nextLine();
+		
+		System.out.println("Here are your employees");
+		System.out.println(employee1);
+		System.out.println(employee2);
+		System.out.println(employee3);
+		System.out.println(employee4);
+		System.out.println(employee5);
+	}}
